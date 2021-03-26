171a360334a6043028c2539e03ab3386f2db888a
chaewon <lisa9902@naver.com>
2021-03-26 22:46:33 +0900

    2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from './potato';
+
+function Food({fav}){
+  
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
   <div className="App">
-    <h1>Hello!BAAAMMM!</h1>
-    <Potato /> {/*component*/}
+    <h1>Hello!!</h1>
+    <Food fav="kimchi"/>
+    <Food fav="ramen"/>
+    <Food fav="cake"/>
+    <Food fav="chukumi"/>
   </div>
   );
 }
