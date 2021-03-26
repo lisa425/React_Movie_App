dbb00c2ba8d296fde62302fd017fbc15ab364ad8
chaewon <lisa9902@naver.com>
2021-03-26 23:27:33 +0900

    2.2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from "react";
 
-function Food({fav}){
+function Food({name,picture}){
   
-  return <h1>I like {fav}</h1>;
+  return <div>
+    <img src={picture}/>
+    <h1>I like {name}</h1>
+  </div>;
+
 }
 
+const foodILike = [
+  {
+    name:"Kimchi",
+    image:"https://i.ytimg.com/vi/eTucCw1w6Ak/maxresdefault.jpg"
+  },
+  {
+    name:"Bibimbap",
+    image:"https://mykoreankitchen.com/wp-content/uploads/2013/07/1.Korean-mixed-rice-Bibimbap.jpg"
+  },
+  {
+    name:"ramen",
+    image:"https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg"
+  },
+  {
+    name:"pasta",
+    image:"https://www.saltandlavender.com/wp-content/uploads/2020/04/tomato-goat-cheese-pasta-recipe-1-500x500.jpg"
+  },
+];
 function App() {
   return (
   <div className="App">
     <h1>Hello!!</h1>
-    <Food fav="kimchi"/>
-    <Food fav="ramen"/>
-    <Food fav="cake"/>
-    <Food fav="chukumi"/>
+    {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/>
+    ))}
   </div>
   );
 }
