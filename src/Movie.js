41856b1a16df6e01674b7dc463e2677bf08691d6
chaewon <lisa9902@naver.com>
2021-04-03 23:54:05 +0900

    4.5

--- src/Movie.js
+++ src/Movie.js
@@ -3,18 +3,20 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({year,title,summary,poster,genres}){
-    return <div className="movies_movie">
+    return (
+    <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movie_data">
             <h3 className="movie_title">{title}</h3>
             <h5 className="movie_year">{year}</h5>
-            <p className="movie_summary">{summary}</p>
+            <p className="movie_summary">{summary.slice(0,140)}...</p>
             <ul className="movie_genres">{genres.map((genre,index) => (
                 <li key={index} className="genres_genre">{genre}</li>
                 ))}
             </ul>
         </div>
-    </div>;
+    </div>
+    );
 };
 
 Movie.propTypes={
