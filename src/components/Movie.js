b9564825244f1e14523fe1c2c1733fbd00012ecf
chaewon <lisa9902@naver.com>
2021-04-04 22:07:47 +0900

    6.3

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({year,title,summary,poster,genres}){
     return (
-    <div className="movie">
-        <img src={poster} alt={title} title={title}/>
-        <div className="movie_data">
-            <h3 className="movie_title">{title}</h3>
-            <h5 className="movie_year">{year}</h5>
-            <p className="movie_summary">{summary.slice(0,140)}...</p>
-            <ul className="movie_genres">{genres.map((genre,index) => (
-                <li key={index} className="genres_genre">{genre}</li>
-                ))}
-            </ul>
-        </div>
-    </div>
+        <Link to={{
+            pathname: "/movie-detail",
+            state: {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}> 
+            <div className="movie">
+                <img src={poster} alt={title} title={title}/>
+                <div className="movie_data">
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <p className="movie_summary">{summary.slice(0,140)}...</p>
+                    <ul className="movie_genres">{genres.map((genre,index) => (
+                        <li key={index} className="genres_genre">{genre}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </Link>
     );
 };
 
